86cfe403399ea922657b4bda992aba5e3242e8f5
simonryrie <simonryrie@gmail.com>
2023-05-26 22:35:15 +0100

    4th test passed

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,20 +1,27 @@
 const { Layout } = require("../templates.js");
+const { getSession } = require("../model/session.js")
 
 function get(req, res) {
-  /**
-   * [1] Read session ID from cookie
-   * [2] Get session from DB
-   * [3] If the session exists render a log out form
-   * [4] This should submit a request to `POST /log-out`
-   * [5] Else render the sign up/log in links
-   */
   const title = "Confess your secrets!";
-  const content = /*html*/ `
+  let content = ""
+  const sid = req.signedCookies.sid;
+  const session = getSession(sid);
+  if (session && session.user_id) {
+    content = /*html*/ `
+    <div class="Cover">
+      <h1>${title}</h1>
+      <form method="POST" action="/log-out"><button type=submit>Log out</button></form>
+    </div>
+  `;
+  } else {
+    // request is not authenticated
+    content = /*html*/ `
     <div class="Cover">
       <h1>${title}</h1>
       <nav><a href="/sign-up">Sign up</a> or <a href="/log-in">log in</a></nav>
     </div>
   `;
+  }
   const body = Layout({ title, content });
   res.send(body);
 }
--- src/routes/log-out.js
+++ src/routes/log-out.js
@@ -1,11 +1,10 @@
+const { removeSession } = require("../model/session.js");
+
 function post(req, res) {
-  /**
-   * [1] Get the session ID from the cookie
-   * [2] Remove that session from the DB
-   * [3] Remove the session cookie
-   * [4] Redirect back home
-   */
-  res.status(500).send("");
+  const sid = req.signedCookies.sid;
+  removeSession(sid);
+  res.clearCookie("sid");
+  res.redirect("/");
 }
 
 module.exports = { post };
